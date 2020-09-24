258567647a8b70b6636a62e6573f26d8f2807865
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2020-09-23 23:40:10 -0700

    test

--- core/src/com/hirshi001/multiplayerrotmg/Rotmg.java
+++ core/src/com/hirshi001/multiplayerrotmg/Rotmg.java
@@ -21,7 +21,7 @@ public class Rotmg extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-
+		//
 		/* Create OrthographicCamera */
 		camera = new OrthographicCamera(size.width, size.height);
 
